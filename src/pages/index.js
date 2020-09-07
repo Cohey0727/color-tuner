5e52e5b5b1e8041131e7d5402b6fc8e09c046658
Kohei <ohayousagi.ac.kook0727@gmail.com>
2020-09-08 00:12:01 +0900

    master

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,51 @@
-import React from "react"
+import React, { useState } from "react";
+import Color from "color";
 
 export default function Home() {
-  return <div>Hello world!</div>
+  const [state, setState] = useState({
+    baseColor: "#FF0000",
+    whiten: 0,
+    blacken: 0,
+    lighten: 0,
+    darken: 0,
+    saturate: 0,
+  });
+
+  const changeValue = (key) => (e) => {
+    setState({ ...state, [key]: e.target.value });
+  };
+
+  const { baseColor, whiten, blacken, lighten, darken, saturate } = state;
+  let color;
+  try {
+    color = Color(baseColor);
+  } catch {
+    color = Color("#FFF");
+  }
+  return (
+    <div>
+      <div>
+        baseColor:{" "}
+        <input onChange={changeValue("baseColor")} value={baseColor} />
+        whiten: <input onChange={changeValue("whiten")} value={whiten} />
+        blacken: <input onChange={changeValue("blacken")} value={blacken} />
+        lighten: <input onChange={changeValue("lighten")} value={lighten} />
+        darken: <input onChange={changeValue("darken")} value={darken} />
+        saturate: <input onChange={changeValue("saturate")} value={saturate} />
+      </div>
+      <div>{JSON.stringify(state)}</div>
+      <div
+        style={{
+          backgroundColor: color
+            .whiten(whiten)
+            .blacken(blacken)
+            .lighten(lighten)
+            .darken(darken)
+            .saturate(saturate),
+          width: 100,
+          height: 100,
+        }}
+      />
+    </div>
+  );
 }
